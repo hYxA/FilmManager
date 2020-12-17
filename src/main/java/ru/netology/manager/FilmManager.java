0ba514822691cb95a0ebfaa6adb5e6059a377e60
hYxA <zybu@live.ru>
2020-12-17 12:52:14 +0300

    create addFilm

--- src/main/java/ru/netology/manager/FilmManager.java
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -2,10 +2,25 @@ package ru.netology.manager;
 
 import ru.netology.domain.FilmItem;
 
+import static java.lang.System.*;
+
 public class FilmManager {
     private FilmItem[] items = new FilmItem[0];
 
-    public void addFilm(FilmItem){
+    /**
+     * Добавление фильма
+     */
+    public void addFilm(FilmItem item) {
+        // создание массива на единицу больше размером
+        int length = items.length + 1;
+        FilmItem[] tmp = new FilmItem[length];
 
+        // копируем поэлементно
+        arraycopy(items, 0, tmp, 0, items.length);
+
+        // кладём последним наш элемент
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
     }
 }
