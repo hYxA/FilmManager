d7050988fa89dc3c0e6a30f8f4b805329de96d14
hYxA <zybu@live.ru>
2020-12-17 12:33:45 +0300

    create files

---
+++ src/main/java/ru/netology/domain/FilmItem.java
@@ -0,0 +1,25 @@
+package ru.netology.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class FilmItem {
+
+    private int id;
+    private int filmId;
+    private String filmName;
+
+
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/ru/netology/manager/FilmManager.java
@@ -0,0 +1,11 @@
+package ru.netology.manager;
+
+import ru.netology.domain.FilmItem;
+
+public class FilmManager {
+    private FilmItem[] items = new FilmItem[0];
+
+    public void addFilm(FilmItem){
+
+    }
+}
